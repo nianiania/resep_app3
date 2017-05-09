3d8f6ee9358a32715c890aa0414114d326b1aa81
kurnia22 <kurniawati.22.nk@gmail.com>
2017-05-09 16:35:08 +0700

    first commit in branch add templateengine

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var bodyParser = require('body-parser')
+var exphbs = require("express-handlebars")
 
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
 
+app.engine('hbs', exphbs({defaultLayout: 'main', extname:'.hbs'}));
+app.set('view engine','.hbs')
+
 
 //=================routing============
 app.route('/')
 	.get(function(req,res){
-		res.sendFile(path.join(__dirname + '/view/home.html'))
+		res.render('home');
 	})
 	.post(function(req, res){
 		var angka_1 = req.body.angka1
@@ -22,17 +26,25 @@ app.route('/')
 
 		if(angka_1 == "" && angka_2 == ""){
 			console.log("error masukkan angka 1 dan angka 2")
+			res.send("error masukkan angka 1 dan angka 2")
 		}else if (angka_1 == ""){
 			console.log("error masukkan angka 1")
+			res.send("error masukkan angka 1")
 		}else if (angka_2 == ""){
 			console.log("error masukkan angka 2")
+			res.send("error masukkan angka 2")
 		}else{ 
 			penjumlahan(angka_1, angka_2)
 
 		}
 		function penjumlahan(a, b){
-			var total = parseInt(a) + parse(b)
-			res.send("total" + a + "+" + b + "=" + total)
+			var total = parseInt(a) + parseInt(b)
+			// res.send("total" + a + "+" + b + "=" + total)
+
+			res.render("hasil",{
+				judul:"hasil penjumlahan",
+				hasil_penjumlahan: total
+			})
 		}
 
 	})
